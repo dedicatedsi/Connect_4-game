5888828805ac2906e8e38ee190071af1810bd876
dedicatedsi <68413193+dedicatedsi@users.noreply.github.com>
2022-04-13 19:41:18 +0530

    Update connect4_game.py

--- Connect_4 game/Project code/connect4_game.py
+++ Connect_4 game/Project code/connect4_game.py
@@ -4,7 +4,6 @@ import pygame
 pygame.init()
 
 #colours
-
 white = (255, 255, 255)
 red = (255, 0, 0)
 yellow = (255, 255, 0)
@@ -21,9 +20,7 @@ errorsound = pygame.mixer.Sound("error.mp3")
 pos_x = [50, 150, 250, 350, 450, 550, 650]
 pos_y = [50, 150, 250, 350, 450, 550]
 
-
 #To create game window
-
 def create_screen():
     screen = pygame.display.set_mode((700, 600))
     screen.fill(background)
@@ -39,7 +36,6 @@ def create_screen():
 
 
 #Checks for vacancy in the column
-
 def col_empty(screen, x):
     if screen.get_at((x, 50)) == white:
         return True
@@ -47,7 +43,6 @@ def col_empty(screen, x):
 
 
 #Gets vacant row
-
 def get_row(screen, x):
     for y in range(550, 49, -100):
         if screen.get_at((x, y)) == white:
@@ -66,18 +61,22 @@ def get_x_pos(screen, x, y):
 
 def horizontal_4(screen, x_coordinate, y_coordinate, colour):
     coin = 1
+    coordinates_list = [[x_coordinate, y_coordinate]]
     prev_x_coordinate = x_coordinate - 100
-    while prev_x_coordinate >= 50 and screen.get_at((prev_x_coordinate, y_coordinate)) == colour:
+    while prev_x_coordinate >= 50 and screen.get_at((prev_x_coordinate, y_coordinate)) == colour and coin != 4:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([prev_x_coordinate, y_coordinate])
         prev_x_coordinate -= 100
     next_x_coordinate = x_coordinate + 100
-    while next_x_coordinate <= 650 and screen.get_at((next_x_coordinate, y_coordinate)) == colour:
+
+    while next_x_coordinate <= 650 and screen.get_at((next_x_coordinate, y_coordinate)) == colour and coin != 4:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([next_x_coordinate, y_coordinate])
         next_x_coordinate += 100
+    if coin == 4:
+        for pair in coordinates_list:
+            add_star(screen, pair[0], pair[1])
+        return True
     return False
 
 
@@ -85,18 +84,16 @@ def horizontal_4(screen, x_coordinate, y_coordinate, colour):
 
 def vertical_4(screen, x_coordinate, y_coordinate, colour):
     coin = 1
-    prev_y_coordinate = y_coordinate - 100
-    while prev_y_coordinate >= 50 and screen.get_at((x_coordinate, prev_y_coordinate)) == colour:
-        coin += 1
-        if coin == 4:
-            return True
-        prev_y_coordinate -= 100
+    coordinates_list = [[x_coordinate, y_coordinate]]
     next_y_coordinate = y_coordinate + 100
-    while next_y_coordinate <= 550 and screen.get_at((x_coordinate, next_y_coordinate)) == colour:
+    while next_y_coordinate <= 550 and screen.get_at((x_coordinate, next_y_coordinate)) == colour and coin != 4:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([x_coordinate, next_y_coordinate])
         next_y_coordinate += 100
+    if coin == 4:
+        for pair in coordinates_list:
+            add_star(screen, pair[0], pair[1])
+        return True
     return False
 
 
@@ -104,46 +101,60 @@ def vertical_4(screen, x_coordinate, y_coordinate, colour):
 
 def diagonal_4(screen, x_coordinate, y_coordinate, colour):
     coin = 1
+    coordinates_list = [[x_coordinate, y_coordinate]]
     prev_x_coordinate = x_coordinate - 100
     prev_y_coordinate = y_coordinate - 100
     while prev_x_coordinate >= 50 and prev_y_coordinate >= 50 and screen.get_at((prev_x_coordinate, prev_y_coordinate)) == colour:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([prev_x_coordinate, prev_y_coordinate])
         prev_x_coordinate -= 100
         prev_y_coordinate -= 100
     next_x_coordinate = x_coordinate + 100
     next_y_coordinate = y_coordinate + 100
     while next_x_coordinate <= 650 and next_y_coordinate <= 550 and screen.get_at((next_x_coordinate, next_y_coordinate)) == colour:
         coin = coin + 1
-        if coin == 4:
-            return True
+        coordinates_list.append([next_x_coordinate, next_y_coordinate])
         next_x_coordinate += 100
         next_y_coordinate += 100
+    if coin == 4:
+        for pair in coordinates_list:
+            add_star(screen, pair[0], pair[1])
+        return True
+
+    coin = 1
     prev_x_coordinate = x_coordinate - 100
     next_y_coordinate = y_coordinate + 100
-    coin = 1
+    coordinates_list = [[x_coordinate, y_coordinate]]
     while prev_x_coordinate >= 50 and next_y_coordinate <= 550 and screen.get_at((prev_x_coordinate, next_y_coordinate)) == colour:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([prev_x_coordinate, next_y_coordinate])
         prev_x_coordinate -= 100
         next_y_coordinate += 100
     next_x_coordinate = x_coordinate + 100
     prev_y_coordinate = y_coordinate - 100
     while next_x_coordinate <= 650 and prev_y_coordinate >= 50 and screen.get_at((next_x_coordinate, prev_y_coordinate)) == colour:
         coin += 1
-        if coin == 4:
-            return True
+        coordinates_list.append([next_x_coordinate, prev_y_coordinate])
         next_x_coordinate += 100
         prev_y_coordinate -= 100
+    if coin == 4:
+        for pair in coordinates_list:
+            add_star(screen, pair[0], pair[1])
+        return True
     return False
 
 
-#To check for winning case
+#Highlighting the won coins
+def add_star(screen, x_coordinate, y_coordinate):
+    image = pygame.image.load(r'C:\Users\hp\OneDrive\Desktop\GitRepository\Connect4Python Project\FinalStar2.png')
+    screen.blit(image, (x_coordinate - 32, y_coordinate - 30))
+    pygame.display.update()
 
+
+#To check for winning case
 def winning_move(screen, x_coordinate, y_coordinate, colour):
     return horizontal_4(screen, x_coordinate, y_coordinate, colour) or vertical_4(screen, x_coordinate, y_coordinate, colour) or diagonal_4(screen, x_coordinate, y_coordinate, colour)
+
 
 def full_board(screen):
     for x in pos_x:
@@ -151,15 +162,12 @@ def full_board(screen):
             return False
     return True
 
+font = pygame.font.Font("freesansbold.ttf", 45)
+
+
 #To display the winner
-
 def end_message(screen,turn):
-     font = pygame.font.Font("freesansbold.ttf", 45)
-     
-     if full_board(screen):
-         text = font.render("game is draw", True, green, blue)
-
-     elif turn % 2 == 0:
+     if turn % 2 == 0:
          text = font.render("Congratulations!! Player1 won", True, green, blue)
      else:
          text = font.render("Congratulations!! Player2 won", True, green, blue)
@@ -199,9 +207,12 @@ while running:
                         winsound.play()
                         end_message(screen, turn)
                         break
+                    elif full_board(screen):
+                        text = font.render("Game is draw!!", True, green, blue)
+                        textRect = text.get_rect()
+                        textRect.center = (700//2,600//2)
+                        screen.blit(text,textRect)
+                        pygame.display.update()
                     turn += 1
                 else:
                     errorsound.play()
-   
-
-
